914cf952f467dec9f3201732030db4011315a878
YocelynPrieto <yocelyncarolina2@gmail.com>
2019-09-27 19:29:29 -0300

    avance hasta la pregunta 5

--- DesarrolloPreguntas.R
+++ DesarrolloPreguntas.R
@@ -1,4 +1,31 @@
 listaDeNumeros<-list(2,5,6,2,1,5,6,10,11,20,15)
 listaDeNumeros[5]
 
+#Pregunta 1
 listaDeNumeros[0]
+listaDeNumeros[13]
+listaDeNumeros[-5]
+
+#si reemplazo el 5 por un 0 aparece list() en la consola.
+#si reemplazo por un numero mayor aparece NULL
+#Si reemplazo por -5 me ejecuta todos los numeros de la lista excepto el que esta en la posicion 5
+
+#Pregunta 2
+unlist(listaDeNumeros[5])
+if (listaDeNumeros[5]){
+  unlist
+}
+
+#con unlist aparece [1] 1
+
+#Pregunta 3
+listaDeNumeros[5]<-12
+#se ejecuta el texto "listaDeNumeros[5]<-12" en la consola
+
+#Pregunta 4
+length(listaDeNumeros)
+
+#se reemplaza "nombre_variable" por "listaDeNumeros"
+#me ejecuta el numero de variables de la lista
+
+#Pregunta 5
